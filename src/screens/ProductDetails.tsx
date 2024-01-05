59b7355b990c6027a19795b0fac36fce5325442f
HERYORDEJY <heryordejy.dev@gmail.com>
2024-01-05 12:57:29 +0100

    Product details in progress

--- src/screens/ProductDetails.tsx
+++ src/screens/ProductDetails.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
 import ScreenNavBar from '~/components/screen/ScreenNavBar.tsx';
 import CustomScreenContainer from '~/navigation/CustomScreenContainer.tsx';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
@@ -8,21 +15,44 @@ import {MainStackParamList} from '~/navigation/types.ts';
 import StarRating from 'react-native-star-rating-widget';
 import {globalStyles} from '~/styles';
 import {svgAssets} from '~/assets';
+import ReadMore from '@fawazahmed/react-native-read-more';
 
-const {StarFillIcon, StarHalfIcon, StarEmptyIcon, FilterIcon, ChevronUpIcon} =
-  svgAssets;
+const {
+  StarFillIcon,
+  StarHalfIcon,
+  StarEmptyIcon,
+  FilterIcon,
+  ChevronUpIcon,
+  DownloadIcon,
+} = svgAssets;
 
-interface Props {
-  //
-}
-
-export default function ProductDetails(props: Props): React.JSX.Element {
+export default function ProductDetails(): React.JSX.Element {
   const navigation =
     useNavigation<
       NativeStackNavigationProp<MainStackParamList, 'ProductDetails'>
     >();
   const route = useRoute<RouteProp<MainStackParamList, 'ProductDetails'>>();
   const product = route.params?.product;
+
+  const renderBadge = useCallback(
+    (title: string, color: string, containerStyle?: ViewStyle) => {
+      return (
+        <View style={[styles.badge, containerStyle, {backgroundColor: color}]}>
+          <Text style={[styles.badgeText]}>{title}</Text>
+        </View>
+      );
+    },
+    [],
+  );
+
+  const renderDownloadButton = useCallback(() => {
+    return (
+      <View style={[styles.downloadButton]}>
+        <Text style={[styles.downloadButtonText]}>Download</Text>
+        <DownloadIcon />
+      </View>
+    );
+  }, []);
 
   return (
     <CustomScreenContainer>
@@ -33,37 +63,105 @@ export default function ProductDetails(props: Props): React.JSX.Element {
             source={{uri: product.image}}
             style={styles.sectionItemImage}
           />
+          <View style={styles.livestockBadge}>
+            {renderBadge('Livestock', '#DB371F', {
+              paddingHorizontal: 10,
+              paddingVertical: 4,
+            })}
+          </View>
           <View style={styles.sectionItemDesc}>
-            <Text style={styles.name}>{product.name}</Text>
-            <View style={styles.ratingWrapper}>
-              <StarRating
-                animationConfig={{scale: 1}}
-                starSize={16}
-                rating={Number(product.reviews.rating)}
-                onChange={() => {}}
-                StarIconComponent={({type}) => {
-                  if (type === 'empty') {
-                    return <StarEmptyIcon width={16} height={16} />;
-                  } else if (type === 'half') {
-                    return <StarHalfIcon width={16} height={16} />;
-                  } else if (type === 'full') {
-                    return <StarFillIcon width={16} height={16} />;
-                  } else {
-                    return <StarEmptyIcon width={16} height={16} />;
-                  }
-                }}
-                starStyle={styles.starStyle}
-              />
-              <Text style={styles.ratingCount}>
-                ({product.reviews.comments.length})
-              </Text>
+            <View style={styles.nameWrapper}>
+              <Text style={styles.name}>{product.name}</Text>
+              {renderBadge(
+                'Wholesale',
+                globalStyles.colors.colorsSecondarySecondary400,
+              )}
             </View>
+            <View style={styles.sellerWrapper}>
+              <View style={styles.sellerNameWrapper}>
+                <Text style={styles.seller}>Seller:</Text>
+                <Text style={styles.sellerName}>Bola Ahmed T.</Text>
+              </View>
+              <View style={styles.ratingWrapper}>
+                <StarRating
+                  animationConfig={{scale: 1}}
+                  starSize={16}
+                  rating={Number(product.reviews.rating)}
+                  onChange={() => {}}
+                  StarIconComponent={({type}) => {
+                    if (type === 'empty') {
+                      return <StarEmptyIcon width={16} height={16} />;
+                    } else if (type === 'half') {
+                      return <StarHalfIcon width={16} height={16} />;
+                    } else if (type === 'full') {
+                      return <StarFillIcon width={16} height={16} />;
+                    } else {
+                      return <StarEmptyIcon width={16} height={16} />;
+                    }
+                  }}
+                  starStyle={styles.starStyle}
+                />
+                <Text style={styles.ratingCount}>
+                  ({product.reviews.comments.length})
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.descWrapper}>
+              <ReadMore
+                numberOfLines={3}
+                style={styles.description}
+                seeLessStyle={styles.seeLessStyle}
+                seeMoreStyle={styles.seeLessStyle}
+                seeMoreText={'Read More'}
+                seeLessText={'Read Less'}>
+                {
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                }
+              </ReadMore>
+            </View>
+
             <View style={styles.currencyWrapper}>
               <Text style={styles.currency}>
                 {product.price.currency}
                 {product.price.value.toLocaleString('en-US')}
               </Text>
               <Text style={styles.unit}>(Per {product.price.unit})</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.warehouseSection}>
+          <View style={styles.warehouseHeader}>
+            <View style={styles.warehouseDivider} />
+            <Text style={styles.warehouseHeaderTitle}>
+              WAREHOUSE INFORMATION
+            </Text>
+            <View style={styles.warehouseDivider} />
+          </View>
+
+          <View style={styles.warehouseWrapper}>
+            <View style={styles.warehouseInfo}>
+              <Text style={styles.warehouseInfoText}>
+                Warehouse distribution
+              </Text>
+              <View style={styles.warehouseInfoValueWrapper}>
+                <Text style={styles.warehouseInfoValue}>
+                  Gennee Warehousing
+                </Text>
+                <Text style={styles.ton}>20.00 Ton</Text>
+              </View>
+            </View>
+            <View style={styles.warehouseInfo}>
+              <Text style={styles.warehouseInfoText}>
+                Warehouse certificate
+              </Text>
+              <View style={styles.warehouseInfoValueWrapper}>
+                <Text style={styles.warehouseInfoValue}>
+                  FFM+ aggregation.png
+                </Text>
+                {renderDownloadButton()}
+              </View>
             </View>
           </View>
         </View>
@@ -76,25 +174,30 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 20,
     paddingVertical: 24,
+    // backgroundColor: 'orange',
   },
   sectionItem: {
-    rowGap: 8,
     backgroundColor: globalStyles.colors.white,
-    borderRadius: 8,
+    // borderRadius: 8,
+    marginBottom: 24,
+
+    overflow: 'hidden',
   },
   sectionItemDesc: {
-    rowGap: 12,
-    paddingHorizontal: 8,
-    paddingBottom: 8,
+    overflow: 'hidden',
+    flex: 1,
+    borderWidth: 1,
+    borderColor: globalStyles.colors.colorsGrayGray25,
+    borderTopWidth: 0,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
   },
   sectionItemImage: {
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     aspectRatio: 16 / 9,
   },
-  listStyle: {
-    backgroundColor: globalStyles.colors.red,
-  },
+  listStyle: {},
   contentContainer: {
     rowGap: 20,
     paddingVertical: 20,
@@ -111,16 +214,24 @@ const styles = StyleSheet.create({
     fontSize: 10,
     fontFamily: globalStyles.fontFamily.italic,
     lineHeight: 13.41,
+    color: globalStyles.colors.foundationGreyGrey300,
   },
   currencyWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
     columnGap: 4,
+    backgroundColor: globalStyles.colors.colorsSecondarySecondary25,
+    paddingLeft: 14,
+    paddingTop: 16,
+    paddingBottom: 15,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
   },
   starStyle: {marginRight: 0, marginLeft: 0},
   name: {
     lineHeight: 28,
     fontSize: 21,
+    fontFamily: globalStyles.fontFamily.medium,
   },
   searchWrapper: {
     flexDirection: 'row',
@@ -147,5 +258,148 @@ const styles = StyleSheet.create({
   columnWrapperStyle: {
     columnGap: 20,
     justifyContent: 'center',
+  },
+  description: {
+    fontSize: 12,
+    color: globalStyles.colors.foundationGreyGrey300,
+  },
+  seeLessStyle: {
+    color: globalStyles.colors.colorsPrimaryPrimary400,
+    fontSize: 12,
+    fontFamily: globalStyles.fontFamily.medium,
+    lineHeight: 12 * 1.53,
+  },
+  descWrapper: {
+    marginBottom: 17,
+    paddingHorizontal: 16,
+  },
+  badge: {
+    backgroundColor: undefined,
+    paddingHorizontal: 7,
+    paddingVertical: 2,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  sellerWrapper: {
+    flexDirection: 'row',
+    marginHorizontal: 16,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 8,
+    marginBottom: 16,
+    paddingBottom: 16,
+    borderColor: '#F1F1F1',
+    borderBottomWidth: 1,
+  },
+  sellerNameWrapper: {
+    flexDirection: 'row',
+    columnGap: 8,
+  },
+  seller: {
+    color: globalStyles.colors.foundationGreyGrey300,
+  },
+  sellerName: {
+    alignSelf: 'center',
+  },
+  nameWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+  badgeText: {
+    color: globalStyles.colors.white,
+    fontSize: 12,
+    fontFamily: globalStyles.fontFamily.semiBold,
+    lineHeight: 12 * 1.53,
+  },
+  warehouseSection: {
+    rowGap: 8,
+  },
+  warehouseHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 8,
+  },
+  warehouseTitle: {
+    fontSize: 14,
+    lineHeight: 14.41,
+    fontFamily: globalStyles.fontFamily.semiBold,
+    color: globalStyles.colors.colorsPrimaryPrimary400,
+  },
+  warehouseWrapper: {
+    rowGap: 24,
+    padding: 16,
+    backgroundColor: globalStyles.colors.white,
+    borderColor: globalStyles.colors.colorsGrayGray25,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  warehouse: {
+    fontSize: 14,
+    lineHeight: 14.41,
+    fontFamily: globalStyles.fontFamily.semiBold,
+    color: globalStyles.colors.colorsPrimaryPrimary400,
+  },
+  warehouseName: {
+    fontSize: 14,
+    lineHeight: 14.41,
+    fontFamily: globalStyles.fontFamily.semiBold,
+    color: globalStyles.colors.colorsPrimaryPrimary400,
+  },
+  warehouseDivider: {
+    height: 1,
+    flex: 1,
+    backgroundColor: globalStyles.colors.colorsGrayGray25,
+  },
+  warehouseHeaderTitle: {
+    fontSize: 12,
+    color: globalStyles.colors.colorsGrayGray300,
+    fontFamily: globalStyles.fontFamily.medium,
+    lineHeight: 18.38,
+  },
+  warehouseInfo: {
+    rowGap: 8,
+  },
+  warehouseInfoText: {
+    lineHeight: 18.38,
+    fontSize: 12,
+    fontFamily: globalStyles.fontFamily.medium,
+    color: globalStyles.colors.colorsSecondarySecondary600,
+  },
+  warehouseInfoValue: {
+    color: '#898B87',
+    lineHeight: 21,
+    // textTransform: 'capitalize',
+  },
+  warehouseInfoValueWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  downloadButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 4,
+  },
+  downloadButtonText: {
+    color: globalStyles.colors.colorsPrimaryPrimary400,
+    fontFamily: globalStyles.fontFamily.medium,
+    lineHeight: 18.38,
+  },
+  ton: {
+    color: '#1F1F1F',
+    fontFamily: globalStyles.fontFamily.medium,
+    lineHeight: 21,
+  },
+  livestockBadge: {
+    position: 'absolute',
+    top: 16,
+    right: 16,
   },
 });
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -7,9 +7,14 @@ export const colors = {
   blue: '#0000FF',
   yellow: '#FFFF00',
   colorsSecondarySecondary25: '#F0F5EC',
+  colorsSecondarySecondary400: '#6D9E3F',
+  colorsSecondarySecondary600: '#004D24',
+  colorsGrayGray25: '#EAEAEA',
+  colorsGrayGray300: '#585858',
   colorsGrayGray800: '#090909',
   colorsPrimaryPrimary400: '#006D33',
   colorsGrayGray50: '#BEBEBE',
+  foundationGreyGrey300: '#686868',
 
   screenBackground: '#F8F8F8',
 };
